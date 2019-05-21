fa04d8c6c76a1b6c6b37ba04ff8bf62022b98519
chrome <hackerdownload@gmail.com>
2019-05-20 22:19:52 -0300

    Iniciando projeto. Configuração com o VSCode App Core criado iniciando tela de Login

---
+++ core/urls.py
@@ -0,0 +1,20 @@
+from django.urls import path
+from core import views
+from core.models import LogMessage
+from django.conf import settings
+from django.conf.urls.static import static
+
+
+home_list_view = views.HomeListView.as_view(
+    queryset=LogMessage.objects.order_by("-log_date")[:5],  # :5 limits the results to the five most recent
+    context_object_name="message_list",
+    template_name="core/index.html",
+)
+
+
+urlpatterns = [
+    path("", home_list_view, name="home"),
+    path("about", views.about, name="about"),
+    path("contact", views.contact, name="contact"),
+    path("log/", views.log_message, name="log"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
