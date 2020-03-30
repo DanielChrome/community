e6235c0bfbee77d942fcd2294edb6c1d7ed0f15c
Daniel Chrome <hackerdownload@gmail.com>
2020-03-30 15:37:51 -0300

    improved profile about page

---
+++ social/static/script/profile.js
@@ -0,0 +1,31 @@
+function openTab(evt, tabName) {
+    var i, x, tablinks;
+    x = document.getElementsByClassName("content-tab");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tab");
+    for (i = 0; i < x.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" is-active", "");
+    }
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " is-active";
+}
+
+function clickMenu(evt, menuName) {
+    var i, x, menulinks;
+    x = document.getElementsByClassName("content-menu-about");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    menulinks = document.getElementsByClassName("menu-about");
+    for (i = 0; i < x.length; i++) {
+        menulinks[i].className = menulinks[i].className.replace(" is-active", "");
+    }
+    document.getElementById(menuName).style.display = "block";
+    //evt.currentTarget.getElementsByClassName("menu-about"). className += " is-active";
+    menulinks = evt.currentTarget.getElementsByClassName("menu-about");
+    for (i = 0; i < x.length; i++) {
+        menulinks[i].className = menulinks[i].className += " is-active";;
+    }
+}
